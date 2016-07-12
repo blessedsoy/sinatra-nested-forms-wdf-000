7812b34a24984b49f8c26fea6cd3b3c62b95d24b
blessedsoy <unmigong@gmail.com>
2016-07-12 18:14:36 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+  PIRATES = []
+
+  def initialize (info_hash)
+    @name = info_hash[:name]
+    @weight = info_hash[:weight]
+    @height = info_hash[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES 
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+  @@all = []
+
+  def initialize (opt_hash)
+    @name = opt_hash[:name]
+    @type = opt_hash[:type]
+    @booty = opt_hash[:booty]
+    @@all << self
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
 end
